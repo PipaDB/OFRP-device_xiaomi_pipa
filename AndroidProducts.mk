8deca476f3713e3002f395f04ed9d7d6391b1bb5
qzcs3 <osamatb.2008@gmail.com>
2025-12-23 12:04:43 +0100

    AndroidProducts: elish => pipa

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_elish.mk
+    $(LOCAL_DIR)/twrp_pipa.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_elish-user \
-    twrp_elish-userdebug \
-    twrp_elish-eng
+    twrp_pipa-user \
+    twrp_pipa-userdebug \
+    twrp_pipa-eng
