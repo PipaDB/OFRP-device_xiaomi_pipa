e3521703771970e751dea42ca14578bb44d060f3
qzcs3 <osamatb.2008@gmail.com>
2025-12-23 11:29:39 +0100

    makefile: twrp_elish.mk => twrp_pipa.mk

--- twrp_pipa.mk
+++ twrp_pipa.mk
@@ -6,7 +6,7 @@
 #
 
 # Release name
-PRODUCT_RELEASE_NAME := elish
+PRODUCT_RELEASE_NAME := pipa
 
 # 引用默认的编译配置文件，core_64_bit必须先于其他引用，否则不生效
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -18,16 +18,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit from elish device
-$(call inherit-product, device/xiaomi/elish/device.mk)
+# Inherit from pipa device
+$(call inherit-product, device/xiaomi/pipa/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := elish
-PRODUCT_NAME := twrp_elish
+PRODUCT_DEVICE := pipa
+PRODUCT_NAME := twrp_pipa
 PRODUCT_BRAND := Xiaomi
-#PRODUCT_MODEL := M2105K81AC
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := Xiaomi Pad 5 Pro
+PRODUCT_MODEL := Xiaomi Pad 6
